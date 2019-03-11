dcaef698eda696f9a64609801c559b8d879ecec6
JonPaulBoyd <jonpaulboyd@hotmail.co.uk>
2019-03-11 20:21:10 +0100

    Preprocessing data analysis

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import logging
 import pandas as pd
 from os import path
+import time
+from contextlib import contextmanager
 from filehandler import Filehandler
 from preprocessor import Preprocessor
+
+
+@contextmanager
+def timer(title):
+    t0 = time.time()
+    yield
+    print("{} - done in {:.0f}s".format(title, time.time() - t0))
+
 
 def process_dataset(filehandler, modeller, dataset_path):
     dataset = filehandler.read_csv(dataset_path)  # Reload dataset to ensure no scaling on scaling successive preds
@@ -10,11 +20,13 @@ def process_dataset(filehandler, modeller, dataset_path):
 
 def main():
     filehandler = Filehandler()
-    Preprocessor()
+    with timer("Preprocessor"):
+        Preprocessor()
 
 
 if __name__ == '__main__':
     import logging.config
     log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging.conf')
     logging.config.fileConfig(log_file_path)
-    main()
+    with timer("Full model run"):
+        main()
--- preprocessor.py
+++ preprocessor.py
@@ -24,12 +24,149 @@ class Preprocessor:
     def data_analysis(self):
         print('_' * 80, "=== Dataset Info ===", sep='\n')
         print('Shape {}'.format(self.dataset_raw.shape))
-        print(self.dataset_raw.columns.values, '_' * 80, sep='\n')
-        print(self.dataset_raw.info(), '_' * 80, sep='\n')
-        print(self.dataset_raw.head(), '_' * 80, sep='\n')
-        print(self.dataset_raw.tail(), '_' * 80, sep='\n')
-        print(self.dataset_raw.describe(), '_' * 80, sep='\n')
-        print(self.dataset_raw.describe(include=['O']), '_' * 80, sep='\n')
+        print('\n', 'Features', self.dataset_raw.columns.values)
+        for col in self.dataset_raw:
+            print(col, len(self.dataset_raw[col].unique()))
+
+        print('\n', 'Label Binary Count', self.dataset_raw['label_binary'].value_counts())
+        print('\n', 'Label Type Count', self.dataset_raw.groupby('label').label_binary.count(), '\n')
+
+        print(self.dataset_raw[['label', 'duration']].groupby(['label'], as_index=False).mean().sort_values(by='duration',
+                                                                                                       ascending=False), '_'*80, sep='\n')
+        print(
+            self.dataset_raw[['label', 'label_binary','protocol_type']].groupby(['label', 'protocol_type'], as_index=False).count().sort_values(by=['label','protocol_type'],
+                                                                                                          ascending=True),
+            '_' * 80, sep='\n')
+
+        print(
+            self.dataset_raw[['label', 'label_binary','service']].groupby(['label', 'service'], as_index=False).count().sort_values(by=['label','service'],
+                                                                                                          ascending=True),
+            '_' * 80, sep='\n')
+
+        print(
+            self.dataset_raw[['label', 'label_binary', 'flag']].groupby(['label', 'flag'],
+                                                                           as_index=False).count().sort_values(
+                by=['label', 'flag'],
+                ascending=True),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'src_bytes']].groupby(['label'], as_index=False).mean().sort_values(by='src_bytes',
+                                                                                                        ascending=False),
+          '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'dst_bytes']].groupby(['label'], as_index=False).mean().sort_values(
+            by='dst_bytes',
+            ascending=False),
+              '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'wrong_fragment']].groupby(['label'], as_index=False).mean().sort_values(
+            by='wrong_fragment',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'urgent']].groupby(['label'], as_index=False).mean().sort_values(
+            by='urgent',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'hot']].groupby(['label'], as_index=False).mean().sort_values(
+            by='hot',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'num_failed_logins']].groupby(['label'], as_index=False).mean().sort_values(
+            by='num_failed_logins',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'logged_in']].groupby(['label'], as_index=False).count().sort_values(
+            by='logged_in',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'num_compromised']].groupby(['label'], as_index=False).count().sort_values(
+            by='num_compromised',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'root_shell']].groupby(['label'], as_index=False).count().sort_values(
+            by='root_shell',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'su_attempted']].groupby(['label'], as_index=False).count().sort_values(
+            by='su_attempted',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'num_root']].groupby(['label'], as_index=False).count().sort_values(
+            by='num_root',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'num_file_creations']].groupby(['label'], as_index=False).count().sort_values(
+            by='num_file_creations',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'num_shells']].groupby(['label'], as_index=False).mean().sort_values(
+            by='num_shells',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'num_access_files']].groupby(['label'], as_index=False).mean().sort_values(
+            by='num_access_files',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'num_outbound_cmds']].groupby(['label'], as_index=False).mean().sort_values(
+            by='num_outbound_cmds',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'is_host_login']].groupby(['label'], as_index=False).count().sort_values(
+            by='is_host_login',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'is_guest_login']].groupby(['label'], as_index=False).count().sort_values(
+            by='is_guest_login',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'count']].groupby(['label'], as_index=False).count().sort_values(
+            by='count',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'srv_count']].groupby(['label'], as_index=False).count().sort_values(
+            by='srv_count',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'serror_rate']].groupby(['label'], as_index=False).mean().sort_values(
+            by='serror_rate',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'srv_serror_rate']].groupby(['label'], as_index=False).mean().sort_values(
+            by='srv_serror_rate',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'rerror_rate']].groupby(['label'], as_index=False).mean().sort_values(
+            by='rerror_rate',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+        print(self.dataset_raw[['label', 'srv_rerror_rate']].groupby(['label'], as_index=False).mean().sort_values(
+            by='srv_rerror_rate',
+            ascending=False),
+            '_' * 80, sep='\n')
+
+
+
+
 
     def clean_label(self):
         self.dataset_raw['label'] = self.dataset_raw['label'].str.rstrip('.')
