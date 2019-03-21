a3b3165ab952de4c1426739cd4894d0deb881a3f
jonpaulboyd@hotmail.co.uk <jonpaulboyd@hotmail.co.uk>
2019-03-21 19:43:27 +0100

    Preprocess refinement

--- preprocessor.py
+++ preprocessor.py
@@ -22,7 +22,7 @@ class Preprocessor:
     def impute_categorical_feature_with_blank(dataset):
         dataset.fillna('', inplace=True)
 
-    def data_analysis(self):
+    def core_analysis(self):
         print('--- Shape')
         print('\tRow count:\t', '{}'.format(self.total_rows))
         print('\tColumn count:\t', '{}'.format(self.dataset.shape[1]))
@@ -32,13 +32,13 @@ class Preprocessor:
         for idx, val in series.iteritems():
             print('\t{}: {} ({:6.3f}%)'.format(idx, val, ((val / self.total_rows) * 100)))
 
-        print('\n--- Row count by attack group')
-        series = self.dataset['attack_group'].value_counts()
+        print('\n--- Row count by attack category')
+        series = self.dataset['attack_category'].value_counts()
         for idx, val in series.iteritems():
             print('\t{}: {} ({:6.3f}%)'.format(idx, val, ((val / self.total_rows) * 100)))
 
-        print('\n--- Row count by attack group/label')
-        df = self.dataset.groupby(['attack_group', 'label', ])[['label_binary']].count()
+        print('\n--- Row count by attack category/label')
+        df = self.dataset.groupby(['attack_category', 'label', ])[['label_binary']].count()
         df = df.rename(columns={'label_binary': 'Count'})
         df['Percent'] = (df['Count'] / self.total_rows) * 100
         df_flat = df.reset_index()
@@ -48,23 +48,12 @@ class Preprocessor:
         for col in self.dataset:
             print('\t{} ({})'.format(col, len(self.dataset[col].unique())))
 
-    def data_discovery(self):
-        self.total_rows = self.dataset.shape[0]
-        print('_' * 40, ' Data Discovery - Full Dataset', '_' * 40, '\n')
-        self.data_analysis()
-
-        print('\n--- Duplicates by attack_group/label')
+    def evaluate_duplicates(self):
+        print('\n--- Duplicates by attack category/label')
         df = self.dataset.groupby(self.dataset.columns.tolist()).size().reset_index(name='duplicates')
         df['duplicates'] = df['duplicates'] - 1
-        df_flat = df.groupby(['attack_group', 'label', ])[['duplicates']].sum().reset_index()
+        df_flat = df.groupby(['attack_category', 'label', ])[['duplicates']].sum().reset_index()
         print(df_flat)
-
-        print('\n', '_' * 40, ' Data Discovery - Dataset Without Duplicates', '_' * 40, '\n')
-        self.dataset.drop_duplicates(keep='first', inplace=True)
-        self.total_rows = self.dataset.shape[0]
-        self.data_analysis()
-
-
 
     def clean_label(self):
         self.dataset['label'] = self.dataset['label'].str.rstrip('.')
@@ -84,7 +73,7 @@ class Preprocessor:
             (self.dataset['label'] == 'spy') | (self.dataset['label'] == 'teardrop') |
             (self.dataset['label'] == 'warezclient') | (self.dataset['label'] == 'warezmaster')
         ]
-        choices = ['normal', 'intrusion']
+        choices = [0, 1]
         self.dataset['label_binary'] = np.select(conditions, choices, default='na')
 
     def set_attack_group(self):
@@ -103,7 +92,20 @@ class Preprocessor:
             (self.dataset['label'] == 'portsweep') | (self.dataset['label'] == 'satan')
         ]
         choices = ['normal', 'dos', 'u2r', 'r2l', 'probe']
-        self.dataset['attack_group'] = np.select(conditions, choices, default='na')
+        self.dataset['attack_category'] = np.select(conditions, choices, default='na')
+
+    def prepare_output_variant_01(self):
+        print('prepare output')
+        self.dataset.drop('is_host_login', inplace=True, axis=1)
+
+    def corr(self):
+        correlation = self.dataset.corr()
+        plt.figure(figsize=(40, 40))
+        sns.heatmap(correlation, vmax=.8, square=True, annot=True, cmap='cubehelix')
+
+        plt.title('Correlation between different fearures')
+
+        plt.show()
 
     def preprocess(self):
         self.filehandler = Filehandler()
@@ -111,7 +113,23 @@ class Preprocessor:
         self.clean_label()
         self.set_binary_label()
         self.set_attack_group()
-        self.data_discovery()
+
+        # Analysis of full dataset
+        self.total_rows = self.dataset.shape[0]
+        print('_' * 40, ' Data Discovery - Full Dataset', '_' * 40, '\n')
+        self.core_analysis()
+
+        # Manage duplicates
+        self.evaluate_duplicates()
+        self.dataset.drop_duplicates(keep='first', inplace=True)
+
+        # Analysis of dataset without duplicates
+        print('\n', '_' * 40, ' Data Discovery - Dataset Without Duplicates', '_' * 40, '\n')
+        self.total_rows = self.dataset.shape[0]
+        self.core_analysis()
+
+        self.corr()
+        self.prepare_output_variant_01()
 
 
 
